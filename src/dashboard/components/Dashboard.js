f89932ca86c181e47659eb77a08518c022711ae9
Andreas Drangel <andreas.drangel@sellpy.se>
2018-01-22 23:53:47 +0100

    add roll and azimuth charts

--- src/dashboard/components/Dashboard.js
+++ src/dashboard/components/Dashboard.js
@@ -10,7 +10,7 @@ import { VictoryLine, VictoryChart, VictoryTheme } from 'victory'
 
 import openSocket from 'socket.io-client'
 
-const Chart = ({data}) => (
+const Chart = ({data, domainY}) => (
   <div style={{
     width: 800
   }}>
@@ -19,12 +19,12 @@ const Chart = ({data}) => (
     >
       <VictoryLine
         width={800}
-        domain={{y: [-Math.PI, Math.PI]}}
+        domain={{y: domainY}}
         style={{
           data: { stroke: COLOR.secondary },
           parent: { border: '1px solid white' }
         }}
-        data={data.map(({pitch}, i) => ({x: i, y: pitch}))}
+        data={data}
       />
     </VictoryChart>
   </div>
@@ -91,13 +91,10 @@ class Dashboard extends Component {
             padding: 10
           }}>
             {selectedDrone && <TestForm onSubmit={this.onSubmit} />}
-            <div style={{
-              color: 'white'
-            }}>
-              {'SIZE IS ' + selectedDroneData.length}
-            </div>
           </div>
-          <Chart data={selectedDroneData} />
+          <Chart data={selectedDroneData.map(({pitch}, i) => ({x: i, y: pitch}))} domainY={[-Math.PI, Math.PI]} />
+          <Chart data={selectedDroneData.map(({roll}, i) => ({x: i, y: roll}))} domainY={[-Math.PI, Math.PI]} />
+          <Chart data={selectedDroneData.map(({azimuth}, i) => ({x: i, y: azimuth}))} domainY={[-Math.PI, Math.PI]} />
         </div>
       </div>
     )
