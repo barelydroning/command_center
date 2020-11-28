5152bfa2814803d2534ed66af65aec0b135757bd
Andreas Drangel <andreas@provement.io>
2020-11-28 21:12:19 +0100

    add motor commands to rover

--- src/dashboard/components/RoverCommands.js
+++ src/dashboard/components/RoverCommands.js
@@ -16,6 +16,11 @@ import COLOR from '../../common/COLOR'
 class RoverCommands extends Component {
   constructor (props) {
     super(props)
+
+    this.motorCommandsSubmit = ({ A, B }) => {
+      const { dispatch, selectedRover, socket } = this.props
+      dispatch(sendCommand(socket, selectedRover, JSON.stringify({ type: 'motors', command: { A: parseFloat(A), B: parseFloat(B) }})))
+    }
   }
 
   render () {
@@ -30,10 +35,25 @@ class RoverCommands extends Component {
         flexDirection: 'row'
       }}>
         <KillSwitch dispatch={dispatch} socket={socket} selected={selectedRover} />
+        <MotorCommand selected={selectedRover} onSubmit={this.motorCommandsSubmit} />
       </div>
     )
   }
 }
+
+let MotorCommand = ({ handleSubmit, onSubmit }) => (
+  <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Field name='A' text='Motor A' component={Input} />
+      <Field name='B' text='Motor B' component={Input} />
+      <Button text='Confirm' onClick={handleSubmit(onSubmit)} />
+    </form>
+  </div>
+)
+
+MotorCommand = reduxForm({
+  form: 'motor'
+})(connect()(MotorCommand))
 
 export default connect(state => ({
   selectedRover: state.dashboard.selectedRover
