8847b3ea16334737fffea3a5e51cb2ae7e8cca8a
Andreas Drangel <andreas@provement.io>
2020-12-05 00:19:40 +0100

    add rover svg

--- src/dashboard/components/Dashboard.js
+++ src/dashboard/components/Dashboard.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {
   Title,
-  SmallTitle
+  SmallTitle,
+  ConnectionLight
 } from '../../common/components'
 import COLOR from '../../common/COLOR'
 import {
@@ -16,14 +17,15 @@ import {
 } from '../actions'
 import DroneCommands from './DroneCommands'
 import RoverCommands from './RoverCommands'
+import roverImage from '../../svg/rover.svg'
 
 import openSocket from 'socket.io-client'
 
 class Dashboard extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
-    this.state = ({socket: openSocket('http://localhost:3001')})
+    this.state = ({ socket: openSocket('http://localhost:3001') })
 
     this.state.socket.on('connect', () => {
       this.props.dispatch(connectClient())
@@ -41,16 +43,16 @@ class Dashboard extends Component {
       this.props.dispatch(setRovers(rovers))
     })
 
-    this.state.socket.on('drone_data', ({drone, ...rest}) => {
+    this.state.socket.on('drone_data', ({ drone, ...rest }) => {
       drone === this.props.selectedDrone && this.props.dispatch(addDroneData(rest))
     })
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.state.socket.emit('connect_client')
   }
 
-  render () {
+  render() {
     const { socket } = this.state
     const {
       dispatch,
@@ -64,57 +66,60 @@ class Dashboard extends Component {
     return (
       <div style={{
         backgroundColor: COLOR.primary,
-        minHeight: window.innerHeight,
-        paddingTop: 50,
+        height: '100vh',
+        width: '100vw',
         fontFamily: '"Noah"'
       }}>
-        <Title text={'Command center'} style={{
-          color: COLOR.secondary,
-          textTransform: 'uppercase',
-          backgroundColor: COLOR.third
-        }} />
+        <div
+          style={{
+            padding: 20
+          }}
+        >
+          <div
+            style={{
+              display: 'flex'
+            }}
+          >
+            <ConnectionLight isConnected={isClientConnected} robot={selectedDrone || selectedRover} />
 
-        <ConnectionLight isConnected={isClientConnected} drone={selectedDrone || selectedRover} />
+            <Title text={'Command center'} style={{
+              color: COLOR.secondary,
+              textTransform: 'uppercase',
+              backgroundColor: COLOR.third,
+              justifyContent: 'center',
+              flex: 1
+            }} />
+          </div>
 
-        <div style={{
-          display: 'flex',
-          flexDirection: 'row'
-        }}>
+          
+
           <div style={{
             display: 'flex',
-            flexDirection: 'column'
+            flexDirection: 'row'
           }}>
-            <div>
-              <SmallTitle text='Drones' />
-              {availableDrones.map(drone => <Robot key={drone} onClick={() => dispatch(selectDrone(drone))} text={drone} selected={selectedDrone === drone} />)}
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column'
+            }}>
+              <div>
+                <SmallTitle text='Drones' />
+                {availableDrones.map((drone, index) => <Robot key={drone} onClick={() => dispatch(selectDrone(drone))} text={index + 1} selected={selectedDrone === drone} />)}
+              </div>
+              <div>
+                <SmallTitle text='Rovers' />
+                {availableRovers.map((rover, index) => <Robot key={rover} onClick={() => dispatch(selectRover(rover))} text={index + 1} selected={selectedRover === rover} />)}
+              </div>
             </div>
-            <div>
-              <SmallTitle text='Rovers' />
-              {availableRovers.map(rover => <Robot key={rover} onClick={() => dispatch(selectRover(rover))} text={rover} selected={selectedRover === rover} />)}
-            </div>
+            {selectedDrone && <DroneCommands socket={socket} />}
+            {selectedRover && <RoverCommands socket={socket} />}
           </div>
-          {selectedDrone && <DroneCommands socket={socket} />}
-          {selectedRover && <RoverCommands socket={socket} />}
         </div>
       </div>
     )
   }
 }
 
-const ConnectionLight = ({isConnected, drone}) => (
-  <div style={{
-    width: 30,
-    height: 30,
-    borderRadius: 30,
-    borderWidth: 2,
-    margin: 20,
-    borderColor: 'white',
-    borderStyle: 'solid',
-    backgroundColor: (isConnected && drone) ? '#15C56A' : (isConnected ? 'yellow' : 'red')
-  }} />
-)
-
-const Robot = ({text, onClick, selected}) => (
+const Robot = ({ text, onClick, selected }) => (
   <div style={{
     backgroundColor: selected ? COLOR.secondary : COLOR.third,
     color: selected ? COLOR.primary : COLOR.fourth,
@@ -125,13 +130,22 @@ const Robot = ({text, onClick, selected}) => (
     padding: 30,
     maxWidth: 300,
     margin: 30,
-    fontSize: '1.2em',
+    fontSize: 40,
     cursor: 'pointer',
-    letterSpacing: 2
+    letterSpacing: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   }}
     onClick={onClick}
   >
-    {text}
+    <img src={roverImage} width={60} />
+    <span
+      style={{
+        marginLeft: 20
+      }}
+    >#{text}</span>
   </div>
 )
 
